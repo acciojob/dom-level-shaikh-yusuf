741e8c4e295802f3469c3629737b8b0191aceb9f
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-30 09:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 
-const ul=document.getElementsByTagName('ul');
+const ul=document.getElementsByTagName('ul')[0];
 let displayText;
 
 for(let i=0;i<ul.children.length;i++){
 	let node=ul.children[i]
 	if(node.id=='level'){
-		displayText="The level of the element is:"+ (i+1);
+		displayText="The level of the element is:"+ (i);
 	}
 }
 
